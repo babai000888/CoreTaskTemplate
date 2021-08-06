235bb48586b27cc4058467889e760741a08c95aa
babai000888 <47792371+babai000888@users.noreply.github.com>
2021-08-06 17:35:01 +0300

    JDBC готов

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -16,19 +16,23 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
     }
 
-    void closeConnection() throws SQLException {
-        if (statement != null) {
-            statement.close();
-        }
-        if (preparedStatement != null) {
-            preparedStatement.close();
-        }
-        if (connection != null) {
-            connection.close();
+    private void closeConnection() {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(" closeConnection ERROR\n" + e.getMessage());
         }
     }
 
-    public void createUsersTable() throws SQLException {
+    public void createUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
                 "  `id` BIGINT NOT NULL AUTO_INCREMENT," +
                 "  `name` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NULL," +
@@ -41,14 +45,13 @@ public class UserDaoJDBCImpl implements UserDao {
             statement.execute(sql);
             System.out.println("Created table " + TABLE_NAME);
         } catch (SQLException e) {
-            System.out.println("Can not create table " + TABLE_NAME);
-            e.printStackTrace();
+            System.out.println(" createUsersTable ERROR\n" + e.getMessage());
         } finally {
             closeConnection();
         }
     }
 
-    public void dropUsersTable() throws SQLException {
+    public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS "+ TABLE_NAME;
         try {
             Connection connection = Util.getConnection();
@@ -56,15 +59,14 @@ public class UserDaoJDBCImpl implements UserDao {
             statement.execute(sql);
             System.out.println("Dropped table " + TABLE_NAME);
         } catch (SQLException e) {
-            System.out.println("Can not drop table " + TABLE_NAME);
-            e.printStackTrace();
+            System.out.println("dropUsersTable ERROR\n" + e.getMessage());
         } finally {
             closeConnection();
         }
     }
 
 
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
+    public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO " + TABLE_NAME +
                 " (`id`, `name`, `lastName`, `age`) " +
                 "   VALUES (DEFAULT, ?, ?, ?)";
@@ -77,14 +79,13 @@ public class UserDaoJDBCImpl implements UserDao {
             preparedStatement.executeUpdate();
             System.out.println("User " + name + "/" + lastName + "/" + age + " created");
         } catch (SQLException e) {
-            System.out.println("Can not save user " + name + "/" + lastName + "/" + age);
-            e.printStackTrace();
+            System.out.println("saveUser ERROR\n" + e.getMessage());
         } finally {
             closeConnection();
         }
     }
 
-    public void removeUserById(long id) throws SQLException {
+    public void removeUserById(long id) {
         String sql = "DELETE FROM "+ TABLE_NAME + " WHERE id = (" + id + ")";
         try {
             Connection connection = Util.getConnection();
@@ -92,13 +93,13 @@ public class UserDaoJDBCImpl implements UserDao {
             int numDeleted = preparedStatement.executeUpdate();
             System.out.println("Amount of deleted users with id " + id + " : " + numDeleted);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("removeUserById ERROR\n" + e.getMessage());
         } finally {
             closeConnection();
         }
     }
 
-    public List<User> getAllUsers() throws SQLException {
+    public List<User> getAllUsers() {
         List<User> allUsers = new ArrayList<>();
         String sql = "SELECT * FROM "+ TABLE_NAME;
         try {
@@ -115,14 +116,14 @@ public class UserDaoJDBCImpl implements UserDao {
             }
             System.out.println("Received " + allUsers.size() + " users");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("getAllUsers ERROR\n" + e.getMessage());
         } finally {
             closeConnection();
         }
         return allUsers;
     }
 
-    public void cleanUsersTable() throws SQLException {
+    public void cleanUsersTable() {
         String sql = "TRUNCATE TABLE "+ TABLE_NAME;
         try {
             Connection connection = Util.getConnection();
@@ -130,7 +131,7 @@ public class UserDaoJDBCImpl implements UserDao {
             statement.execute(sql);
             System.out.println("Tables cleared");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("cleanUsersTable ERROR\n" + e.getMessage());
         } finally {
             closeConnection();
         }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,37 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDao test = new UserDaoJDBCImpl();
     public void createUsersTable() {
-
+test.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        test.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        test.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        test.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
+
+        test.getAllUsers();
         return null;
     }
 
     public void cleanUsersTable() {
-
+        test.cleanUsersTable();
     }
 }
